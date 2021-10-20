c82214542fb6cfba7515c37a793038b5b0879083
Anna Bush <annabush092@gmail.com>
2021-10-20 19:25:08 -0400

    food table

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,24 @@
+class FoodsController < ApplicationController
+    def index 
+        @foods = Food.all
+    end
+
+    def new
+      @food = Food.new
+    end
+
+    def create
+      @food = Food.new(food_params)
+      if @food.save
+        redirect_to foods_path(@food)
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def food_params
+      params.require(:food).permit(:name)
+    end
+end
